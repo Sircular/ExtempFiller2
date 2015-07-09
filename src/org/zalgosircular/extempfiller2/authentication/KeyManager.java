8e34ab15e6b2e7b0a974a469ba949f0dd7873013
Sircular <sircular17@gmail.com>
2015-07-08 23:31:49 -0600

    Made the key manager sane

--- src/org/zalgosircular/extempfiller2/authentication/KeyManager.java
+++ src/org/zalgosircular/extempfiller2/authentication/KeyManager.java
@@ -12,21 +12,12 @@ import java.util.regex.Pattern;
  * Created by Walt on 7/8/2015.
  */
 public class KeyManager {
-    private static String workingDir;
     private final static String FILENAME = ".extempKeys";
     private static HashMap<String, String> keyMap;
 
-    private static boolean initWorkingDir() {
-        if (workingDir == null) {
-            workingDir = KeyManager.class.getProtectionDomain().getCodeSource().getLocation().getFile();
-            return true;
-        }
-        return false;
-    }
-
     private static boolean initMap() {
         if (keyMap == null) {
-            keyMap = new HashMap<>();
+            keyMap = new HashMap<String, String>();
             return true;
         }
         return false;
@@ -35,10 +26,9 @@ public class KeyManager {
     public static String getKey(String account) {
         if (keyMap == null) {
             try {
-                initWorkingDir();
                 initMap();
                 // initialize the map for the first time
-                Path keyFilePath = Paths.get(workingDir, FILENAME);
+                Path keyFilePath = Paths.get(FILENAME);
                 if (Files.exists(keyFilePath)) {
                     // we can actually initialize the keys, so we may as well
                     // create the object
