dcc3b510cfcdb781a6d6adf80b442e1ad369366c
Sircular <sircular17@gmail.com>
2015-07-19 20:27:51 -0600

    ENMLFormatter now outputs proper xhtml, so that we don't have to use hacky regular expressions anymore.

--- src/org/zalgosircular/extempfiller2/research/formatting/ENMLFormatter.java
+++ src/org/zalgosircular/extempfiller2/research/formatting/ENMLFormatter.java
@@ -1,5 +1,7 @@
 package org.zalgosircular.extempfiller2.research.formatting;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
 import org.zalgosircular.extempfiller2.research.Article;
@@ -12,7 +14,13 @@ public class ENMLFormatter extends HTMLFormatter implements ArticleFormatter {
     @Override
     public String format(Article article) {
         // we'll just reuse the methods from the HTMLFormatter
-        final Element body = new Element(Tag.valueOf("en-note"), "");
+
+        // we have to make this document because that's
+        // the only way to get the body element to output
+        // xhtml.
+        final Document doc = Jsoup.parse("");
+        final Element body = doc.body();
+        doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
         final Element infoDiv = generateInfoDiv(article);
         final Element contentDiv = new Element(Tag.valueOf("div"), "");
         contentDiv.append(normalizeContent(article.getHTML()));
@@ -23,9 +31,8 @@ public class ENMLFormatter extends HTMLFormatter implements ArticleFormatter {
                         System.getProperty("line.separator") +
                         "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">" +
                         System.getProperty("line.separator");
-        String bodyOutput = body.toString();
-        // we have to do this for evernote; wtf
-        return header + bodyOutput.replaceAll("<br( /)?>", "<br></br>");
+        String bodyOutput = "<en-note>"+body.html()+"</en-note>";
+        return header + bodyOutput;
     }
 
     @Override
