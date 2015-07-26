c4fc7d0796a1dce846a3e1287e8f349711258635
Sircular <sircular17@gmail.com>
2015-07-25 19:00:03 -0600

    It is now possible to choose to start the cli instead by specifying "cli" on the command line.

--- src/org/zalgosircular/extempfiller2/ExtempFiller2.java
+++ src/org/zalgosircular/extempfiller2/ExtempFiller2.java
@@ -3,6 +3,7 @@ package org.zalgosircular.extempfiller2;
 import org.zalgosircular.extempfiller2.messaging.InMessage;
 import org.zalgosircular.extempfiller2.messaging.OutMessage;
 import org.zalgosircular.extempfiller2.research.ResearchWorker;
+import org.zalgosircular.extempfiller2.ui.CLI;
 import org.zalgosircular.extempfiller2.ui.gui.GUI;
 
 import java.util.concurrent.BlockingQueue;
@@ -20,7 +21,32 @@ public class ExtempFiller2 {
         workerThread.start();
 
         // initialize the UI
-        GUI gui = new GUI(inQueue, outQueue);
-        gui.run();
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("gui")) {
+                runGUI(inQueue, outQueue);
+            } else if (args[0].equalsIgnoreCase("cli")) {
+                runCLI(inQueue, outQueue);
+            } else {
+                // output a debug message
+                System.out.println(String.format("Unrecognized parameter: %s.\n", args[0])+
+                "Recognized options: gui, cli.\n"+
+                "Initializing GUI (default.)");
+                runGUI(inQueue, outQueue);
+            }
+        } else {
+            runGUI(inQueue, outQueue);
+        }
+    }
+
+    private static void runCLI(BlockingQueue<InMessage> inQueue,
+                               BlockingQueue<OutMessage> outQueue) {
+        CLI cli = new CLI(inQueue, outQueue);
+        cli.run();
+    }
+
+    private static void runGUI(BlockingQueue<InMessage> inQueue,
+                               BlockingQueue<OutMessage> outQueue) {
+        GUI cli = new GUI(inQueue, outQueue);
+        cli.run();
     }
 }
