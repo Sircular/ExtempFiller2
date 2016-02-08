16a2abc65604bfb30fd0b4d7d4d6aef80165d3c2
Sircular <sircular17@gmail.com>
2016-02-07 20:14:28 -0700

    Made the program use the new backend

--- src/main/java/org/zalgosircular/extempfiller2/ExtempFiller2.java
+++ src/main/java/org/zalgosircular/extempfiller2/ExtempFiller2.java
@@ -7,6 +7,8 @@ import org.zalgosircular.extempfiller2.research.fetching.ArticleFetcher;
 import org.zalgosircular.extempfiller2.research.fetching.web.ReadabilityArticleFetcher;
 import org.zalgosircular.extempfiller2.research.fetching.web.urls.SEARCH_ENGINE;
 import org.zalgosircular.extempfiller2.research.formatting.ENMLFormatter;
+import org.zalgosircular.extempfiller2.research.formatting.HTMLFormatter;
+import org.zalgosircular.extempfiller2.research.storage.BoxStorage;
 import org.zalgosircular.extempfiller2.research.storage.StorageFacility;
 import org.zalgosircular.extempfiller2.research.storage.evernote.EvernoteStorage;
 import org.zalgosircular.extempfiller2.ui.cli.CLI;
@@ -24,7 +26,7 @@ public class ExtempFiller2 {
         final BlockingQueue<InMessage> inQueue = new ArrayBlockingQueue<InMessage>(1024);
         final BlockingQueue<OutMessage> outQueue = new ArrayBlockingQueue<OutMessage>(1024);
         final ArticleFetcher fetcher = new ReadabilityArticleFetcher(outQueue, SEARCH_ENGINE.GOOGLE);
-        final StorageFacility storage = new EvernoteStorage(outQueue, new ENMLFormatter());
+        final StorageFacility storage = new BoxStorage(outQueue, new HTMLFormatter());
         final ResearchWorker worker = new ResearchWorker(inQueue, outQueue, fetcher, storage);
         worker.getThread().start();
 
